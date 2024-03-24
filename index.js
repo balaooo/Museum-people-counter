723d1d76d0e6396612521e29ad2c7cb5f1cb0c04
Jenny Huang <chenyuh565@gmail.com>
2024-03-24 15:05:47 +0800

    Finish project

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+const saveEl = document.getElementById("save-el");
+const countEl = document.getElementById("count-el");
+let eachCount = 0;
+const countList = document.getElementById("the-table-i-want");
+console.log(countList);
+console.log(countEl);
 
-function increment() {
-    count += 1
-    countEl.textContent = count
+
+const allData = [];
+
+
+
+function increaseCount() {
+    eachCount += 1;
+    countEl.innerText = eachCount;
 }
 
 function save() {
-    let countStr = count + "人" + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
+    let countStr = `最近一次儲存人數${eachCount}人`;
+    saveEl.textContent = countStr;
+    countEl.textContent = 0;
+    const submitTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     
-    count = 0
+
+    //建立人數＆時間戳記資料
+    const savedData = {
+        time: submitTime,
+        count: eachCount
+    };
+    
+
+    allData.push(savedData);
+
+    //每次儲存就渲染到下方的list上
+    countList.innerHTML += `
+        <tr>
+        <td>${savedData.time}</td>
+        <td>${savedData.count}</td>
+        </tr>
+    `;
+    
+    eachCount = 0;
 }
 
